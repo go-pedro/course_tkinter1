136407ce924e239a14f3f6ad66ec44b8a9e5303e
Pedro Goncalves <pgoncalves.pt@gmail.com>
2022-01-30 19:40:21 +0100

    themed tkinter

--- tkinter_1.py
+++ tkinter_1.py
@@ -2,6 +2,7 @@
 
 #import packages
 import tkinter as tk
+from tkinter import ttk
 
 #create the root window
 root = tk.Tk()
@@ -23,6 +24,9 @@ root.iconbitmap('./img/pedras.ico')
 # place a label on the root window
 message = tk.Label(root, text="Hello, World!")
 message.pack()
+
+message_2=ttk.Label(root, text='Themed Label')
+message_2.pack()
 
 # keep the window displaying
 root.mainloop()
