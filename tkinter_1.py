7307a83937d4b539609e4c04375660b25fde0806
Pedro Goncalves <pgoncalves.pt@gmail.com>
2022-01-30 18:14:28 +0100

    add window title

--- tkinter_1.py
+++ tkinter_1.py
@@ -6,6 +6,10 @@ import tkinter as tk
 #create the root window
 root = tk.Tk()
 
+#add title
+root.title('Tkinter Window Demo')
+
+
 # place a label on the root window
 message = tk.Label(root, text="Hello, World!")
 message.pack()
